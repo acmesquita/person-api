5839b9c0f8d17163c3e8fb85d55f55cca3c7051f
Catharina Mesquita <catha.ana.1994@gmail.com>
2024-10-25 12:04:55 -0300

    feat: create contact with person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,19 +1,31 @@
 class PeopleController < ApplicationController
   def create
-    person_params = params.require(:person).permit(:firstName, :lastName)
+    Person.transaction do
+      person_params = params.require(:person).permit(:firstName, :lastName, contacts: [ :kind, :content ])
 
-    # Adapter
-    person_attributes = {
-      first_name: person_params[:firstName],
-      last_name: person_params[:lastName]
-    }
+      # Adapter
+      person_attributes = {
+        first_name: person_params[:firstName],
+        last_name: person_params[:lastName]
+      }
 
-    person = Person.new(person_attributes)
+      person = Person.new(person_attributes)
 
-    if person.save
-      render status: :ok, json: { id: person.id, name: person.name }
-    else
-      render status: :bad_request, json: person.errors.as_json
+      if person.save
+        person_params[:contacts].each do |contact_params|
+          Contact.create(contact_params.merge(person_id: person.id))
+        end
+
+        render status: :ok, json: {
+          id: person.id,
+          name: person.name,
+          contacts: person.contacts.map { |contact| { kind: contact.kind, content: contact.content } }
+        }
+      else
+        render status: :bad_request, json: person.errors.as_json
+      end
     end
+  rescue => exception
+    render json: { error: exception.message }.to_json, status: 500
   end
 end
--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -3,18 +3,38 @@ require "test_helper"
 class PeopleControllerTest < ActionDispatch::IntegrationTest
   test "should create" do
     params = {
-      person: { firstName: "Fulano", lastName: "de tal" }
+      person: {
+        firstName: "Fulano",
+        lastName: "de tal",
+        contacts: [
+          { kind: "phone", content: "(86)9999-9999" }
+        ]
+      }
     }
     post "/people", params: params
     assert_response :success
   end
 
 
-  test "should not create" do
+  test "should not create if lastName don't provided" do
     params = {
       person: { firstName: "Fulano" }
     }
     post "/people", params: params
     assert_response :bad_request
+  end
+
+  test "should not create if kind of contact is invalid" do
+    params = {
+      person: {
+        firstName: "Fulano",
+        lastName: "de tal",
+        contacts: [
+            { kind: "whatsapp", content: "(86)9999-9999" }
+          ]
+     }
+    }
+    post "/people", params: params
+    assert_response 500
   end
 end
