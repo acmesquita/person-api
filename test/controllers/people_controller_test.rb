59e6856e4ed483f1a677dacf34160fbf049eddc0
Catharina Mesquita <catha.ana.1994@gmail.com>
2024-10-25 11:27:09 -0300

    fix: ajust test to people controller

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get people_create_url
+  test "should create" do
+    post "/people", params: { person: { firstName: "Fulano", lastName: "de tal" } }
     assert_response :success
   end
 end
