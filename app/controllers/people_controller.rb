c9f1071a6b780b665445e7c0d376e32a67d00447
Catharina Mesquita <catha.ana.1994@gmail.com>
2024-10-25 10:56:32 -0300

    feat: adding adapter to fix params names

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,14 @@
 class PeopleController < ApplicationController
   def create
-    person_params = params.require(:person).permit(:first_name, :last_name)
+    person_params = params.require(:person).permit(:firstName, :lastName)
 
-    person = Person.new(person_params)
+    # Adapter
+    person_attributes = {
+      first_name: person_params[:firstName],
+      last_name: person_params[:lastName]
+    }
+
+    person = Person.new(person_attributes)
 
     if person.save
       render status: 200, json: { id: person.id, name: person.name }
